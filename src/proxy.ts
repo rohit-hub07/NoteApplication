9432f0bcf883d93c05401eccf93735313aa5894d
rohitsingh <rohitsingh986491@gmail.com>
2026-01-13 18:51:14 +0530

    fixed state

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,18 +1,34 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export function proxy(request: NextRequest) {
+// export function proxy(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
+//   console.log("path inside of middleware: ", path);
+//   const pathName = path === "/login" || path === "/signup" || path === "/home"
+
+//   const token = request.cookies.get('token')?.value || ''
+//   if (pathName && token) {
+//     return NextResponse.redirect(new URL("/home", request.nextUrl))
+//   }
+//   if (!pathName && !token) {
+//     return NextResponse.redirect(new URL("/login", request.nextUrl));
+//   }
+// }
+
+export function proxy(request: NextRequest){
   const path = request.nextUrl.pathname;
-  console.log("path inside of middleware: ", path);
-  const pathName = path === "/login" || path === "/signup" || path === "/home"
+  const protectedPath = path.includes("/createtask") || path.includes("/api/todo/createtodo") || path.includes("/api/todo/deletetodo");
 
   const token = request.cookies.get('token')?.value || ''
-  if (pathName && token) {
-    return NextResponse.redirect(new URL("/home", request.nextUrl))
+
+  if(protectedPath && !token){
+    return NextResponse.json({
+      message: "You are not allowed to perform this action!",
+      success: false,
+    },{status: 403})
   }
-  if (!pathName && !token) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
-  }
+
+  return NextResponse.next();
 }
 
 interface MiddlewareConfig {
@@ -22,7 +38,9 @@ interface MiddlewareConfig {
 export const config: MiddlewareConfig = {
   matcher: [
     "/createtask",
-    "/login",
+    "/api/todo/createtodo",
+    "/api/todo/deletetodo",
+    "/api/todo/show",
     "/signup"
   ]
 }
